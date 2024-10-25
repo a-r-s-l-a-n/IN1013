05c0835c8d561684d4915d081ef707bd68d2a0bf
a-r-s-l-a-n <arslan.shaikh@city.ac.uk>
2024-10-25 14:52:03 +0100

    Added database homework 4

---
+++ HW 4/task1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE petPet (petname VARCHAR(20), owner VARCHAR(45), species
+VARCHAR(15), gender CHAR(1), birth DATE, death DATE, PRIMARY KEY (petname));
+
+CREATE TABLE petEvent (petname VARCHAR(20), eventdate DATE, eventtype 
+VARCHAR(20), remark VARCHAR(255), FOREIGN KEY (petname) REFERENCES
+petPet(petname), PRIMARY KEY (petname,eventdate,eventtype));
